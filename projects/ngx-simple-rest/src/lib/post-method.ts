853c3203e0ff6f489206a128ff219f12d43990be
Oscar Iván <oiprado@gmail.com>
2020-10-15 11:50:01 -0500

    adding content file

---
+++ projects/ngx-simple-rest/src/lib/post-method.ts
@@ -0,0 +1,21 @@
+import { MethodInfo } from "./method-info";
+import { ResourceInfo } from './resource-info';
+import { SimpleRest } from './ngx-simple-rest';
+
+export function PostMethod(methodInfo: MethodInfo) {
+  
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+
+    let originalMethod = descriptor.value;
+
+    descriptor.value = function (...args: any[]) {
+        
+        this.methodInfo = methodInfo;
+        let result = originalMethod.apply(this, args);
+
+        return result;
+    }
+    
+  }
+
+}
